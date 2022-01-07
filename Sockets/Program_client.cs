2e1d680cccb95b995afab3240af69791c25d68b2
SoftOn12 <80104120+SoftOn12@users.noreply.github.com>
2022-01-08 01:05:37 +0300

    Add files via upload

---
+++ Sockets/Program_client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SocketStudy_Client
+{
+    class Program
+    {
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(address), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipep);
+                Console.Write("Введите сообщение: ");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("Ответ сервера: " + builder.ToString());
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Sockets/Program_server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace SocketStudy_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 8005);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(ipep);
+                socket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while(true)
+                {
+                    Socket client = socket.Accept();
+                    //StringBuilder builder = new StringBuilder();
+                    //byte[] TextData = new byte[8192]; // буфер для текста
+
+                    do
+                    {
+                        MemoryStream MemStr = new MemoryStream();
+                        FileStream FileLog = File.Create("logger.log");
+
+                        while (true)
+                        {
+                            byte[] buffer = new byte[4096];
+                            int FileBytes = client.Receive(buffer);
+                            if (FileBytes == 0)
+                            {
+                                break;
+                            }
+                            MemStr.Write(buffer, 0, buffer.Length);
+                        }
+                        MemStr.WriteTo(FileLog);
+                        FileLog.Close();
+                        MemStr.Close();
+                    }
+                    while (client.Available > 0);
+                    Console.WriteLine("Файл получен");
+
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+            catch(Exception exe)
+            {
+                Console.WriteLine(exe.Message);
+            }
+        }
+    }
+}
